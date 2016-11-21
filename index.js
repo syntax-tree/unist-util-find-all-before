e64ef554d194cd9def70c0acf62beb1a38105658
Titus Wormer <tituswormer@gmail.com>
2016-11-21 23:09:44 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,68 +1,41 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:find-all-before
- * @fileoverview Utility to find nodes before another node.
- */
-
 'use strict';
-
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
 
 var is = require('unist-util-is');
 
-/**
- * Find nodes before `index` in `parent` which pass `test`.
- *
- * @param {Node} parent - Parent to search in.
- * @param {number|Node} index - (Position of) node to
- *   search before.
- * @param {*} test - See `wooorm/unist-util-is`.
- * @return {Node?} - A child node of `parent` which pass
- *   `test`.
- */
-function findAllBefore(parent, index, test) {
-    var results = [];
-    var children;
-    var child;
-
-    if (!parent || !parent.type || !parent.children) {
-        throw new Error('Expected parent node');
-    }
-
-    children = parent.children;
-
-    if (index && index.type) {
-        index = children.indexOf(index);
-    }
-
-    if (isNaN(index) || index < 0 || index === Infinity) {
-        throw new Error('Expected positive finite index or child node');
-    }
-
-    /* Performance. */
-    if (index > children.length) {
-        index = children.length;
-    }
-
-    while (index--) {
-        child = children[index];
-
-        if (is(test, child, index, parent)) {
-            results.push(child);
-        }
-    }
-
-    return results;
-}
-
-/*
- * Expose.
- */
-
 module.exports = findAllBefore;
+
+/* Find nodes before `index` in `parent` which pass `test`. */
+function findAllBefore(parent, index, test) {
+  var results = [];
+  var children;
+  var child;
+
+  if (!parent || !parent.type || !parent.children) {
+    throw new Error('Expected parent node');
+  }
+
+  children = parent.children;
+
+  if (index && index.type) {
+    index = children.indexOf(index);
+  }
+
+  if (isNaN(index) || index < 0 || index === Infinity) {
+    throw new Error('Expected positive finite index or child node');
+  }
+
+  /* Performance. */
+  if (index > children.length) {
+    index = children.length;
+  }
+
+  while (index--) {
+    child = children[index];
+
+    if (is(test, child, index, parent)) {
+      results.push(child);
+    }
+  }
+
+  return results;
+}
--- test.js
+++ test.js
@@ -1,140 +1,114 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:find-all-before
- * @fileoverview Test suite for `unit-util-find-all-before`.
- */
-
 'use strict';
 
-/* eslint-env node, mocha */
-
-/*
- * Dependencies.
- */
+/* eslint-env mocha */
 
 var assert = require('assert');
 var mdast = require('mdast');
 var findAllBefore = require('./');
 
-/*
- * Methods.
- */
-
-var dequal = assert.deepEqual;
-
-/*
- * Fixture.
- */
-
-var ast = mdast.parse('Some *emphasis*, **strongness**, and `code`.');
-var paragraph = ast.children[0];
+var tree = mdast.parse('Some *emphasis*, **strongness**, and `code`.');
+var paragraph = tree.children[0];
 var children = paragraph.children;
 
-/*
- * Tests.
- */
-
 describe('unist-util-find-all-before', function () {
-    it('should fail without parent', function () {
-        assert.throws(function () {
-            findAllBefore();
-        }, /Expected parent node/);
-    });
+  it('should fail without parent', function () {
+    assert.throws(
+      function () {
+        findAllBefore();
+      },
+      /Expected parent node/
+    );
+  });
 
-    it('should fail without parent node', function () {
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo'
-            });
-        }, /Expected parent node/);
-    });
+  it('should fail without parent node', function () {
+    assert.throws(
+      function () {
+        findAllBefore({type: 'foo'});
+      },
+      /Expected parent node/
+    );
+  });
 
-    it('should fail without index', function () {
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo',
-                'children': []
-            });
-        }, /Expected positive finite index or child node/);
+  it('should fail without index', function () {
+    assert.throws(
+      function () {
+        findAllBefore({type: 'foo', children: []});
+      },
+      /Expected positive finite index or child node/
+    );
 
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo',
-                'children': []
-            }, -1);
-        }, /Expected positive finite index or child node/);
+    assert.throws(
+      function () {
+        findAllBefore({type: 'foo', children: []}, -1);
+      },
+      /Expected positive finite index or child node/
+    );
 
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo',
-                'children': []
-            }, {
-                'type': 'bar'
-            });
-        }, /Expected positive finite index or child node/);
-    });
+    assert.throws(
+      function () {
+        findAllBefore({type: 'foo', children: []}, {type: 'bar'});
+      },
+      /Expected positive finite index or child node/
+    );
+  });
 
-    it('should fail for invalid `test`', function () {
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo',
-                'children': [{
-                    'type': 'bar'
-                }]
-            }, 1, false);
-        }, /Expected function, string, or node as test/);
+  it('should fail for invalid `test`', function () {
+    assert.throws(
+      function () {
+        findAllBefore({type: 'foo', children: [{type: 'bar'}]}, 1, false);
+      },
+      /Expected function, string, or node as test/
+    );
 
-        assert.throws(function () {
-            findAllBefore({
-                'type': 'foo',
-                'children': [{
-                    'type': 'bar'
-                }]
-            }, 1, true);
-        }, /Expected function, string, or node as test/);
-    });
+    assert.throws(
+      function () {
+        findAllBefore({
+          type: 'foo',
+          children: [{type: 'bar'}]
+        }, 1, true);
+      },
+      /Expected function, string, or node as test/
+    );
+  });
 
-    it('should return the preceding nodes when without `test`', function () {
-        var res = [children[0]];
+  it('should return the preceding nodes when without `test`', function () {
+    var res = [children[0]];
 
-        dequal(findAllBefore(paragraph, children[1]), res);
-        dequal(findAllBefore(paragraph, 1), res);
-        dequal(findAllBefore(paragraph, 0), []);
-    });
+    assert.deepEqual(findAllBefore(paragraph, children[1]), res);
+    assert.deepEqual(findAllBefore(paragraph, 1), res);
+    assert.deepEqual(findAllBefore(paragraph, 0), []);
+  });
 
-    it('should return `[node]` when given a `node` and existing', function () {
-        var res = [children[0]];
+  it('should return `[node]` when given a `node` and existing', function () {
+    var res = [children[0]];
 
-        dequal(findAllBefore(paragraph, 100, children[0]), res);
-        dequal(findAllBefore(paragraph, children[1], children[0]), res);
-        dequal(findAllBefore(paragraph, 1, children[0]), res);
-        dequal(findAllBefore(paragraph, children[0], children[0]), []);
-        dequal(findAllBefore(paragraph, 0, children[0]), []);
-        dequal(findAllBefore(paragraph, 1, children[1]), []);
-    });
+    assert.deepEqual(findAllBefore(paragraph, 100, children[0]), res);
+    assert.deepEqual(findAllBefore(paragraph, children[1], children[0]), res);
+    assert.deepEqual(findAllBefore(paragraph, 1, children[0]), res);
+    assert.deepEqual(findAllBefore(paragraph, children[0], children[0]), []);
+    assert.deepEqual(findAllBefore(paragraph, 0, children[0]), []);
+    assert.deepEqual(findAllBefore(paragraph, 1, children[1]), []);
+  });
 
-    it('should return children when given a `type` and existing', function () {
-        var result = [children[3]];
+  it('should return children when given a `type` and existing', function () {
+    var result = [children[3]];
 
-        dequal(findAllBefore(paragraph, 100, 'strong'), result);
-        dequal(findAllBefore(paragraph, 3, 'strong'), []);
-        dequal(findAllBefore(paragraph, children[4], 'strong'), result);
-        dequal(findAllBefore(paragraph, children[3], 'strong'), []);
-    });
+    assert.deepEqual(findAllBefore(paragraph, 100, 'strong'), result);
+    assert.deepEqual(findAllBefore(paragraph, 3, 'strong'), []);
+    assert.deepEqual(findAllBefore(paragraph, children[4], 'strong'), result);
+    assert.deepEqual(findAllBefore(paragraph, children[3], 'strong'), []);
+  });
 
-    it('should return children when given a `test` and existing', function () {
-        var res = children.slice(4).reverse();
+  it('should return children when given a `test` and existing', function () {
+    var res = children.slice(4).reverse();
 
-        /** Test */
-        function test(node, n) {
-            return n > 3;
-        }
+    assert.deepEqual(findAllBefore(paragraph, 100, test), res);
+    assert.deepEqual(findAllBefore(paragraph, 3, test), []);
+    assert.deepEqual(findAllBefore(paragraph, children[4], test), []);
+    assert.deepEqual(findAllBefore(paragraph, children[3], test), []);
 
-        dequal(findAllBefore(paragraph, 100, test), res);
-        dequal(findAllBefore(paragraph, 3, test), []);
-        dequal(findAllBefore(paragraph, children[4], test), []);
-        dequal(findAllBefore(paragraph, children[3], test), []);
-    });
+    function test(node, n) {
+      return n > 3;
+    }
+  });
 });
