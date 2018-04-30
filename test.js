558c2cf25dbc7b096bc33f7705a20f8d766bb405
Titus Wormer <tituswormer@gmail.com>
2018-04-30 20:03:04 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var test = require('tape');
 var remark = require('remark');
-var findAllBefore = require('./');
+var findAllBefore = require('.');
 
 var tree = remark().parse('Some _emphasis_, **importance**, and `code`.');
 var paragraph = tree.children[0];
