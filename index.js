17e217d374292ae47c068151ec1189c423540497
Titus Wormer <tituswormer@gmail.com>
2020-11-01 17:09:22 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -7,33 +7,31 @@ module.exports = findAllBefore
 function findAllBefore(parent, index, test) {
   var is = convert(test)
   var results = []
-  var children
-  var child
 
   if (!parent || !parent.type || !parent.children) {
     throw new Error('Expected parent node')
   }
 
-  children = parent.children
+  if (typeof index === 'number') {
+    if (index < 0 || index === Infinity) {
+      throw new Error('Expected positive finite number as index')
+    }
+  } else {
+    index = parent.children.indexOf(index)
 
-  if (index && index.type) {
-    index = children.indexOf(index)
-  }
-
-  if (isNaN(index) || index < 0 || index === Infinity) {
-    throw new Error('Expected positive finite index or child node')
+    if (index < 0) {
+      throw new Error('Expected child node or index')
+    }
   }
 
   // Performance.
-  if (index > children.length) {
-    index = children.length
+  if (index > parent.children.length) {
+    index = parent.children.length
   }
 
   while (index--) {
-    child = children[index]
-
-    if (is(child, index, parent)) {
-      results.push(child)
+    if (is(parent.children[index], index, parent)) {
+      results.push(parent.children[index])
     }
   }
 
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 'use strict'
 
-var assert = require('assert')
 var test = require('tape')
 var remark = require('remark')
 var findAllBefore = require('.')
@@ -26,85 +25,140 @@ test('unist-util-find-all-before', function (t) {
     'should fail without parent node'
   )
 
-  t.doesNotThrow(function () {
-    assert.throws(function () {
+  t.throws(
+    function () {
       findAllBefore({type: 'foo', children: []})
-    }, /Expected positive finite index or child node/)
+    },
+    /Expected child node or index/,
+    'should fail without index (#1)'
+  )
 
-    assert.throws(function () {
+  t.throws(
+    function () {
       findAllBefore({type: 'foo', children: []}, -1)
-    }, /Expected positive finite index or child node/)
+    },
+    /Expected positive finite number as index/,
+    'should fail without index (#2)'
+  )
 
-    assert.throws(function () {
+  t.throws(
+    function () {
       findAllBefore({type: 'foo', children: []}, {type: 'bar'})
-    }, /Expected positive finite index or child node/)
-  }, 'should fail without index')
+    },
+    /Expected child node or index/,
+    'should fail without index (#3)'
+  )
 
-  t.doesNotThrow(function () {
-    assert.throws(function () {
+  t.throws(
+    function () {
       findAllBefore({type: 'foo', children: [{type: 'bar'}]}, 1, false)
-    }, /Expected function, string, or object as test/)
+    },
+    /Expected function, string, or object as test/,
+    'should fail for invalid `test` (#1)'
+  )
 
-    assert.throws(function () {
-      findAllBefore(
-        {
-          type: 'foo',
-          children: [{type: 'bar'}]
-        },
-        1,
-        true
-      )
-    }, /Expected function, string, or object as test/)
-  }, 'should fail for invalid `test`')
+  t.throws(
+    function () {
+      findAllBefore({type: 'foo', children: [{type: 'bar'}]}, 1, true)
+    },
+    /Expected function, string, or object as test/,
+    'should fail for invalid `test` (#2)'
+  )
 
-  t.doesNotThrow(function () {
-    assert.deepStrictEqual(findAllBefore(paragraph, children[1]), [children[0]])
-    assert.deepStrictEqual(findAllBefore(paragraph, 1), [children[0]])
-    assert.deepStrictEqual(findAllBefore(paragraph, 0), [])
-  }, 'should return the preceding nodes when without `test`')
+  t.deepEqual(
+    findAllBefore(paragraph, children[1]),
+    [children[0]],
+    'should return the preceding nodes when without `test` (#1)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 1),
+    [children[0]],
+    'should return the preceding nodes when without `test` (#1)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 0),
+    [],
+    'should return the preceding nodes when without `test` (#1)'
+  )
 
-  t.doesNotThrow(function () {
-    assert.deepStrictEqual(findAllBefore(paragraph, 100, children[0]), [
-      children[0]
-    ])
-    assert.deepStrictEqual(findAllBefore(paragraph, children[1], children[0]), [
-      children[0]
-    ])
-    assert.deepStrictEqual(findAllBefore(paragraph, 1, children[0]), [
-      children[0]
-    ])
-    assert.deepStrictEqual(
-      findAllBefore(paragraph, children[0], children[0]),
-      []
-    )
-    assert.deepStrictEqual(findAllBefore(paragraph, 0, children[0]), [])
-    assert.deepStrictEqual(findAllBefore(paragraph, 1, children[1]), [])
-  }, 'should return `[node]` when given a `node` and existing')
+  t.deepEqual(
+    findAllBefore(paragraph, 100, children[0]),
+    [children[0]],
+    'should return `[node]` when given a `node` and existing (#1)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[1], children[0]),
+    [children[0]],
+    'should return `[node]` when given a `node` and existing (#2)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 1, children[0]),
+    [children[0]],
+    'should return `[node]` when given a `node` and existing (#3)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[0], children[0]),
+    [],
+    'should return `[node]` when given a `node` and existing (#4)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 0, children[0]),
+    [],
+    'should return `[node]` when given a `node` and existing (#5)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 1, children[1]),
+    [],
+    'should return `[node]` when given a `node` and existing (#6)'
+  )
 
-  t.doesNotThrow(function () {
-    var result = [children[3]]
+  t.deepEqual(
+    findAllBefore(paragraph, 100, 'strong'),
+    [children[3]],
+    'should return children when given a `type` and existing (#1)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 3, 'strong'),
+    [],
+    'should return children when given a `type` and existing (#2)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[4], 'strong'),
+    [children[3]],
+    'should return children when given a `type` and existing (#3)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[3], 'strong'),
+    [],
+    'should return children when given a `type` and existing (#4)'
+  )
 
-    assert.deepStrictEqual(findAllBefore(paragraph, 100, 'strong'), result)
-    assert.deepStrictEqual(findAllBefore(paragraph, 3, 'strong'), [])
-    assert.deepStrictEqual(
-      findAllBefore(paragraph, children[4], 'strong'),
-      result
-    )
-    assert.deepStrictEqual(findAllBefore(paragraph, children[3], 'strong'), [])
-  }, 'should return children when given a `type` and existing')
+  var result = children.slice(4).reverse()
 
-  t.doesNotThrow(function () {
-    var result = children.slice(4).reverse()
+  t.deepEqual(
+    findAllBefore(paragraph, 100, test),
+    result,
+    'should return children when given a `test` and existing (#1)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, 3, test),
+    [],
+    'should return children when given a `test` and existing (#2)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[4], test),
+    [],
+    'should return children when given a `test` and existing (#3)'
+  )
+  t.deepEqual(
+    findAllBefore(paragraph, children[3], test),
+    [],
+    'should return children when given a `test` and existing (#4)'
+  )
 
-    assert.deepStrictEqual(findAllBefore(paragraph, 100, test), result)
-    assert.deepStrictEqual(findAllBefore(paragraph, 3, test), [])
-    assert.deepStrictEqual(findAllBefore(paragraph, children[4], test), [])
-    assert.deepStrictEqual(findAllBefore(paragraph, children[3], test), [])
-
-    function test(node, n) {
-      return n > 3
-    }
-  }, 'should return children when given a `test` and existing')
+  function test(node, n) {
+    return n > 3
+  }
 
   t.end()
 })
